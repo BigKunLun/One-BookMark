a4ce91337dc5c31036a19302e47b7caaab2d42b3
石佳宁 <shijianing@MacBook-Pro.local>
2018-01-17 15:28:40 +0800

    v0.1, just can be use.

---
+++ assets/js/bookmarks.js
@@ -0,0 +1,97 @@
+var my_bookmarks_favorite_list = [{
+  icon: "search",
+  title: "Google",
+  href: "https://google.com",
+  note: "Google一下，全都知道",
+},{
+  icon: "G",
+  title: "Github",
+  href: "https://github.com/BigKunLun",
+  note: "世界最大的代码仓库，开源应有尽有",
+},{
+  icon: "K",
+  title: "Kaggle",
+  href: "https://kaggle.com",
+  note: "数据建模和数据分析竞赛平台",
+},{
+  icon: "J",
+  title: "Jupyter",
+  href: "http://localhost:8888",
+  note: "JUPYTER NOTEBOOK",
+},{
+  icon: "movie",
+  title: "66电影港",
+  href: "http://www.dygang.net/",
+  note: "生活怎能没有电影？",
+},{
+  icon: "movie",
+  title: "LOL电影天堂",
+  href: "http://www.loldytt.com",
+  note: "资源丰富",
+},{
+  icon: "web",
+  title: "Iplaysoft",
+  href: "http://www.iplaysoft.com/",
+  note: "异次元软件世界，软件改变生活",
+},{
+  icon: "π",
+  title: "少数派",
+  href: "http://sspai.com",
+  note: "基于专业创作社群的数字消费指南平台",
+}]
+
+
+var my_bookmarks_api_list = [{
+  icon: "X",
+  title: "XGBoost",
+  href: "http://xgboost.readthedocs.io/en/latest/python/python_api.html",
+  note: "XGBoost eXtreme Gradient Boosting",
+  category: "version 0.6-4"
+},{
+  icon: "S",
+  title: "Sklearn",
+  href: "http://scikit-learn.org/stable/index.html",
+  note: "Machine Learning in Python",
+  category: "version 0.19.1"
+},{
+  icon: "P",
+  title: "Python3",
+  href: "http://www.runoob.com/python3/python3-tutorial.html",
+  note: "Python API",
+  category: "version 3.6"
+},{
+  icon: "P",
+  title: "PyCookbook",
+  href: "http://python3-cookbook.readthedocs.io/zh_CN/latest/index.html",
+  note: "人生苦短，我用 Python！",
+  category: "version 3"
+},{
+  icon: "L",
+  title: "Linux",
+  href: "http://linuxtools-rst.readthedocs.io/zh_CN/latest/index.html",
+  note: " Linux Tools Quick Tutorial",
+  category: "version Latest"
+}]
+
+var my_bookmarks = [{
+  page_name: "Favorite",
+  page_ico: "favorite",
+  page_list: my_bookmarks_favorite_list
+},{
+  page_name: "API",
+  page_ico: "dashboard",
+  page_list: my_bookmarks_api_list
+}]
+
+_.each(my_bookmarks, function(page){
+  if(page.page_name == "Favorite"){
+    _.map(page.page_list, function(bm,i){
+      bm.category = i
+    })
+  }else{
+    page.page_list = _.sortBy(page.page_list, 'title');
+  }
+  _.each(page.page_list, function(bm){
+    bm.color = _.sample(['purple','red','blue','green','orange'])
+  });
+})
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+  function page_list_view(page_list){
+    var flag
+    for (var i = 0; i < page_list.length; i++) {
+      if(i%4==0){
+        $("#row-container").append("<div id='row" + i + "' class='row'></div")
+        flag = i
+      }
+      html = "<div class='col-lg-3 col-md-6 col-sm-6'> \
+                <a href='"+ page_list[i].href +"'> \
+                  <div class='card card-stats'> \
+                    <div class='card-header' data-background-color='"+page_list[i].color+"'> \
+                      <i class='material-icons'>"+page_list[i].icon+"</i> \
+                    </div> \
+                    <div class='card-content'> \
+                        <p class='category'>" + page_list[i].category + "</p> \
+                        <h3 class='title'> \
+                            " + page_list[i].title + " \
+                        </h3> \
+                    </div> \
+                    <div class='card-footer'> \
+                      <div class='stats'> \
+                        <i class='material-icons' style='top:0'>local_offer</i> \
+                        <a href='#'>"+page_list[i].note+"</a> \
+                    </div> \
+                  </div> \
+                </a> \
+              </div>"
+      $("#row"+flag).append(html)
+    }
+  }
+
+  $("ul.nav").html( _.template($("#nav_list").html(), my_bookmarks));
+
+  page_list_view(my_bookmarks[0].page_list)
+
+  $('ul.nav li').click(function(){
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    var page_name = $(this).find('p')[0].innerHTML;
+    var page_list = _.find(my_bookmarks, function(data){ return data.page_name== page_name}).page_list;
+    $("#row-container").html("");
+    page_list_view(page_list);
+  })
+
+});
